3efd2033d0856ad9608bdedf28499f57cdc21834
florstavri <111541502+florstavri@users.noreply.github.com>
2022-09-22 21:11:39 -0300

    1.7
    actualizacion menu

---
+++ js/navmenu.js
@@ -0,0 +1,8 @@
+function navmenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topNav";
+    }
+} 
